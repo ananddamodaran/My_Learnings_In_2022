fabab2c95e35146057eec91069527d9aefaf4817
Anand Damodaran <anand@damodaran.dev>
2022-02-19 23:12:45 +0530

    adding android

---
+++ android_playground/app/src/main/java/dev/damodaran/android_playground/MainActivity.kt
@@ -0,0 +1,117 @@
+package dev.damodaran.android_playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.damodaran.android_playground.ui.theme.Android_playgroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Android_playgroundTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+
+    // Greeting("Android")
+    val names: List<String> = listOf("World,", "Compose")
+    Greetings()
+    /* Column(modifier = Modifier.padding(vertical = 14.dp)) {
+         for (name in names) {
+             Greeting(name = name)
+         }
+     }*/
+
+
+}
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    //Text(text = "Hello, $name!", modifier = Modifier.padding(24.dp))
+     var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(
+            vertical = 4.dp, horizontal = 8.dp
+        )
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+
+            ) {
+                Text(text = "Hello,")
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    ))
+                 if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+            }
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+
+        }
+
+    }
+
+
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    Android_playgroundTheme {
+        Greeting("Android Android")
+    }
+}
---
+++ android_playground/app/src/main/java/dev/damodaran/android_playground/OnBoardActivity.kt
@@ -0,0 +1,100 @@
+package dev.damodaran.android_playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.damodaran.android_playground.ui.theme.Android_playgroundTheme
+
+class OnBoardActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Android_playgroundTheme {
+                // A surface container using the 'background' color from the theme
+                var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+                if (shouldShowOnboarding) { // Where does this come from?
+                    OnboardingScreen(onContinueClicked={
+
+                        shouldShowOnboarding = !shouldShowOnboarding
+
+                    })
+                } else {
+                    MyApp()
+                }
+
+
+
+            }
+        }
+    }
+}
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.primary
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Composable
+fun Gr3eeting(name: String) {
+    //Text(text = "Hello, $name!", modifier = Modifier.padding(24.dp))
+    var expanded = remember { mutableStateOf(false)}//
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp
+        ) ){
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)
+
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = {  expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+
+        }
+
+    }
+
+
+}
+
+
+@Preview(showBackground = true,widthDp = 320, heightDp = 320)
+@Composable
+fun DefaultPreview2() {
+    Android_playgroundTheme {
+        OnboardingScreen(onContinueClicked={}) // Do nothing on click.
+
+    }
+}
