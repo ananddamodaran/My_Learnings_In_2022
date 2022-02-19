bfb18174baac44d49be1fd3ef0d42a433d64f842
Anand Damodaran <anand@damodaran.dev>
2022-02-19 08:51:55 +0530

    update gradle properties

--- android_playground/app/src/main/java/dev/damodaran/android_playground/MainActivity.kt
+++ android_playground/app/src/main/java/dev/damodaran/android_playground/MainActivity.kt
@@ -3,11 +3,14 @@ package dev.damodaran.android_playground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
@@ -16,10 +19,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.damodaran.android_playground.ui.theme.Android_playgroundTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,39 +44,62 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp() {
 
-       // Greeting("Android")
-        val names: List<String> = listOf("World,", "Compose")
-        Column(modifier = Modifier.padding(vertical = 14.dp)) {
-            for (name in names) {
-                Greeting(name = name)
-            }
-        }
+    // Greeting("Android")
+    val names: List<String> = listOf("World,", "Compose")
+    Greetings()
+    /* Column(modifier = Modifier.padding(vertical = 14.dp)) {
+         for (name in names) {
+             Greeting(name = name)
+         }
+     }*/
 
 
 }
 
 @Composable
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
     //Text(text = "Hello, $name!", modifier = Modifier.padding(24.dp))
-    var expanded = remember { mutableStateOf(false)}//
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-    Surface(color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp
-        ) ){
-    Row(modifier = Modifier.padding(24.dp)) {
-        Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)
+     var expanded by remember { mutableStateOf(false) }
 
-        ) {
-            Text(text = "Hello,")
-            Text(text = name)
-        }
-        OutlinedButton(
-            onClick = {  expanded.value = !expanded.value }
-        ) {
-            Text(if (expanded.value) "Show less" else "Show more")
-        }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(
+            vertical = 4.dp, horizontal = 8.dp
+        )
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
 
-    }
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+
+        }
 
     }
 
